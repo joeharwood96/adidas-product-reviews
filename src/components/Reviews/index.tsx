c7fe7912ed3685d67069b1d916ff94dfae5d706e
Joeharwood-appical <joeharwood3@gmail.com>
2022-02-18 14:23:49 +0100

    Added initial setup for add review modal

--- src/components/Reviews/index.tsx
+++ src/components/Reviews/index.tsx
@@ -48,7 +48,7 @@ export default function Reviews() {
 
             {showAddReview && 
                 <div>
-                    hi
+                    
                 </div>
             }
         </div>
